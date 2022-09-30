87491ca92da3b8e9258062615dd5f9e06f38a299
rkas123 <rkrupeshkumar345@gmail.com>
2022-09-30 11:47:53 +0530

    Add Canny Edge detector in flutter app. Fix the title of Sobel route.

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 
 import 'opencv_widgets/blur.dart';
+import 'opencv_widgets/canny.dart';
 import 'opencv_widgets/gaussianblur.dart';
 import 'opencv_widgets/hough.dart';
-import './widgets/mainpage.dart';
 import 'opencv_widgets/houghlinesprob.dart';
 import 'opencv_widgets/sobel.dart';
+
+import './widgets/mainpage.dart';
 
 List<CameraDescription> _cameras;
 
@@ -29,7 +31,8 @@ class MyApp extends StatelessWidget {
         HoughProb.routeName: (ctx) => HoughProb(_cameras),
         Sobel.routeName: (ctx) => Sobel(_cameras),
         GaussianBlurWidget.routeName: (ctx) => GaussianBlurWidget(_cameras),
-        BlurWidget.routeName: (ctx) => BlurWidget(_cameras)
+        BlurWidget.routeName: (ctx) => BlurWidget(_cameras),
+        Canny.routeName: (ctx) => Canny(_cameras),
       },
     );
   }
